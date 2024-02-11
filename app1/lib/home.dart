423f39cb07a8fa0f005aaed30298f54e9b9ad370
YizhakTime <ydcohen@mail.csuchico.edu>
2024-02-11 12:46:15 -0800

    Adding drawer and camera button

--- app1/lib/home.dart
+++ app1/lib/home.dart
@@ -7,6 +7,21 @@ class MyUser {
   // MyUser({this.id})
 }
 
+//String greeting => "Hello";
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
 class Home extends StatelessWidget {
   const Home({super.key, required this.user});
 
@@ -38,9 +53,46 @@ class Home extends StatelessWidget {
             },
           )
         ],
-        automaticallyImplyLeading: false,
+        automaticallyImplyLeading: true,
         title: const Text(appTitle),
+        centerTitle: true,
         backgroundColor: const Color(0xff6750a4),
+      ),
+      drawer: SizedBox(
+        width: 230,
+        child: Drawer(
+          backgroundColor: Colors.white,
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              const DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Color(0xff6750a4),
+                ),
+                child: Text(
+                  "Options",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+              // ListTile(
+              //   title: const Text("Notes"),
+              //   onTap: () {
+              //     Navigator.pop(context);
+              //   },
+              // ),
+              ListTile(
+                title: const Text("Sign-out"),
+                onTap: () {
+                  const SignOutButton();
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          ),
+        ),
       ),
       body: Center(
         child: Column(
@@ -49,9 +101,15 @@ class Home extends StatelessWidget {
               'Welcome!',
               style: Theme.of(context).textTheme.displaySmall,
             ),
-            const SignOutButton()
+            const SignOutButton(),
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        foregroundColor: Colors.white,
+        backgroundColor: const Color(0xff6750a4),
+        child: const Icon(Icons.camera),
       ),
     );
   }
