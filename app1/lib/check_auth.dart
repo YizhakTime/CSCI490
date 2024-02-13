46d9ff6bfb566e96ddedefd34001168ce8a7988f
YizhakTime <ydcohen@mail.csuchico.edu>
2024-02-13 00:39:38 -0800

    Google Auth works

--- app1/lib/check_auth.dart
+++ app1/lib/check_auth.dart
@@ -1,4 +1,5 @@
 import 'package:app1/home.dart';
+import 'package:firebase_ui_oauth_google/firebase_ui_oauth_google.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
@@ -17,9 +18,48 @@ class CheckAuthState extends StatelessWidget {
           return SignInScreen(
             providers: [
               EmailAuthProvider(),
+              GoogleProvider(
+                  clientId:
+                      "177920090403-9cj3h62c4qn337hn6sr7dp7j2nsdgq7h.apps.googleusercontent.com"), // new
             ],
+            headerBuilder: (context, constraints, shrinkOffset) {
+              return const Padding(
+                padding: EdgeInsets.all(20),
+                child: AspectRatio(
+                  aspectRatio: 1,
+                  // child: Image.asset('flutterfire_300x.png'),
+                ),
+              );
+            },
+            subtitleBuilder: (context, action) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: action == AuthAction.signIn
+                    ? const Text('Welcome to FlutterFire, please sign in!')
+                    : const Text('Welcome to Flutterfire, please sign up!'),
+              );
+            },
+            footerBuilder: (context, action) {
+              return const Padding(
+                padding: EdgeInsets.only(top: 16),
+                child: Text(
+                  'By signing in, you agree to our terms and conditions.',
+                  style: TextStyle(color: Colors.grey),
+                ),
+              );
+            },
+            sideBuilder: (context, shrinkOffset) {
+              return const Padding(
+                padding: EdgeInsets.all(20),
+                child: AspectRatio(
+                  aspectRatio: 1,
+                  // child: Image.asset('apple.png'),
+                ),
+              );
+            },
           );
-        } //No data
+        } //snapshot.hasData
+
         return Home(user: snapshot.data);
       },
     );
